5d2a38dc884b9c4281c53c158804b563dc28a84b
JustinTrautman <justin.trautman@gmail.com>
2017-03-24 19:35:37 -0700

    Update Windows.bat

--- Windows.bat
+++ Windows.bat
@@ -1,15 +1,27 @@
 @echo off
+color 0f
 cls
-:admchk
+:admnchk
 echo Checking for Administrative permissions
 net sessions
-if %errolevel%==0 (
-echo You have Administrative permissions 
+if %errorlevel%==0 (
+echo You have Administrative permissions
 goto :adminhop
 ) else (
 echo Please open as Administrator
 pause
 exit
+)
+:adminhop
+echo Running password policies
+net accounts /minpwlen:9 /maxpwage:20 /uniquepw:1 /lockoutduration:30 /lockoutthreshold:5
+echo password policies have been configured 
+echo minimum password lenght = 9
+echo maximum password age = 20
+echo unique password = 1
+echo lockout duration = 30 minutes
+echo lockout threshold = 5
+pause
 )
 :adminhop
 echo Running password policies
